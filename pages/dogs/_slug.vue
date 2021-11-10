8e35025af1e60e667ca22b447a6eb7c9addcd297
letoast <ciroman@gmail.com>
2021-11-10 10:47:15 +0100

    Setup

---
+++ pages/dogs/_slug.vue
@@ -0,0 +1,36 @@
+<template>
+  <img :src="dog.image" alt="">
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dog: {
+        image: ''
+      }
+    }
+  },
+  async fetch () {
+    const { data } = await this.$axios.get('https://dog.ceo/api/breeds/image/random')
+    this.dog.image = data.message
+  },
+  head () {
+    return {
+      title: this.dog.image,
+      meta: [
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.dog.image
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: this.dog.image
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>Go to /dogs/1</p>
+</template>
+
+<script>
+export default {}
+</script>
